235456cdcab94e83b2036a7d6c44fa45dc68c622
bruno.rvieira2 <bruno.rvieira2@TAT0541216W10-1.senacsp.edu.br>
2018-06-06 14:36:41 -0300

    Calculo da Área do Circulo

---
+++ Fundamentos do Java/src/br/com/java/Áreadocirculo.java
@@ -0,0 +1,28 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class Áreadocirculo {
+
+	public static void main(String[] args) {
+		double area, pi, r;
+		pi = 3.14;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite o valor do raio : ");		
+		r = teclado.nextDouble();
+		r =  r * r; 
+		System.out.println("valor do raio : " + r );
+		System.out.println("valor da area : " + r * pi );
+	
+		
+		
+		
+		
+				
+	
+		
+				
+		
+	}
+
+}
